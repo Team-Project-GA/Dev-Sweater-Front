2258983b05d50ff3d1e459ad3f1acf271a6b4181
kevinjmosley93 <kevinjmosley93@gmail.com>
2020-11-29 13:34:46 -0600

    Latest Commit
    Added Cart Component back to the folder

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const Cart = ({ products, user }) => {
-  console.log('products in cart component:', products)
-  console.log('user in cart component:', user)
+const Cart = () => {
   return (
     <div>
-      <h1>this is the cart page</h1>
+      <h1>this is cart page</h1>
     </div>
   )
 }
